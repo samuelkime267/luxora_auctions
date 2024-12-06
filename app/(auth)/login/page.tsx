022f0739ea7ef138353ad063533b8f60cf21a621
samuelkime267 <samuelkime7@gmail.com>
2024-12-07 00:03:54 +0100

    added suspense

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import LoginForm from "@/features/authentication/components/LoginForm";
 
 export default function Login() {
   return (
     <div className="flex items-center justify-center w-full">
-      <LoginForm />
+      <Suspense>
+        <LoginForm />
+      </Suspense>
     </div>
   );
 }
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import RegistrationForm from "@/features/authentication/components/RegistrationForm";
 
 export default function Register() {
   return (
     <div className="flex items-center justify-center w-full">
-      <RegistrationForm />
+      <Suspense>
+        <RegistrationForm />
+      </Suspense>
     </div>
   );
 }
